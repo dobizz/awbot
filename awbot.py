5aa824bb41a3ea0df9308ccd5a70c0f867f7c829
Ronnie Villanueva <ronnie.code@outlook.com>
2022-01-05 23:06:50 +0800

    Initial update of bot to new game interface
    - modified whole code structure as the old JavaScript code calls are no longer working
    - added selenium-stealth to reduce chance of anti-bot detection

--- awbot.py
+++ awbot.py
@@ -5,272 +5,262 @@ import json
 import time
 import random
 import pathlib
-from threading import Thread
+from itertools import count
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from timeit import default_timer as timer
-from utils import download_chromedriver, has_chromedriver, Release
+from selenium.common.exceptions import NoSuchElementException
+from selenium_stealth import stealth
+from utils import download_chromedriver, has_chromedriver
 
 
-# set Chrome options
-chrome_options = Options()
-chrome_options.add_argument('--log-level=3')
-chrome_options.add_argument('--ignore-certificate-errors')
-chrome_options.add_argument('--ignore-certificate-errors-spki-list')
-chrome_options.add_argument('--ignore-ssl-errors')
-# chrome_options.add_argument('--headless')
-# save session
-pwd = pathlib.Path().absolute()
-chrome_options.add_argument(f"--user-data-dir={pwd}\\chrome-data")
-
-# instantiate Chrome driver with given Chrome options
-driver = webdriver.Chrome(
-    options=chrome_options,
-)
-
-# change page load timeout
-driver.set_page_load_timeout(10)
-
-# save current window handle
-main_window = driver.current_window_handle
+def _print_(text:str) -> None:
+    sys.stdout.write(text)
+    sys.stdout.flush()
 
 
-class AlienWorlds:
-    url = "https://play.alienworlds.io/"
-
-    def __init__(self):
-        self.federation_account = "federation"
-        self.mining_account = "m.federation"
-        self.hyperion_endpoints = ["https://api.waxsweden.org", "https://wax.eosrio.io"]
-        # visit url
-        driver.get(AlienWorlds.url)
-
-
-    def login(self):
-        """
-        Calls javascript code to login user and saves wax account name to instance variable called account
-        """
-        driver.execute_script("""await server_login();""")
-        time.sleep(3)
-        # get wax user
-        self.account = self.get_wax_user()
-
-
-    def get_wax_user(self):
-        """
-        Calls javascript code to retrieve wax account name
-        """
-        wax = driver.execute_script('return wax;')
-        return wax['userAccount']
-
-
-    def get_bag(self) -> list:
-        """
-        Returns list containing the item details of player's bag
-        """
-        bag = driver.execute_script('var bag = await getBag("' + self.mining_account + '" , "' + self.account + '", wax.api.rpc, aa_api); return bag;')
-        return bag
-
-
-    def set_bag(self, bag:list):
-        """
-        Sets the bag of account with given list of items in bag argument
-        """
-        raise NotImplementedError
-        if 0 <= len(bag) <= 3:
-            driver.execute_script(f'await server_setBag("{self.account}", {bag})')
-
-
-    def get_land(self) -> dict:
-        """
-        Returns dictionary containing details of current land
-        """
-        land = driver.execute_script(f'var data = await getLand("{self.federation_account}", "{self.mining_account}", "{self.account}", wax.api.rpc, aa_api); return data;')
-        return land
-
-
-    def get_balance(self) -> str:
-        """
-        Returns a string indicating the TLM balance of player's account
-        """
-        balance = driver.execute_script(f'var balance = await getBalance("{self.account}", wax.api.rpc); return balance;')
-        return balance
-
-
-    def get_mine_delay(self) -> int:
-        """
-        Returns an integer indicating the time in milliseconds before next allowable mine attempt
-        """
-        mine_delay = driver.execute_script(f'var minedelay = await getMineDelay("{self.account}"); return minedelay;')
-        return mine_delay
-
-
-    def get_bag_difficulty(self) -> int:
-        """
-        Returns an integer indicating the current bag difficulty
-        """
-        bag_difficulty = driver.execute_script(f'var difficulty = await getBagDifficulty("{self.account}"); return difficulty')
-        return bag_difficulty
-
-
-    def get_land_difficulty(self) -> int:
-        """
-        Returns an integer indicating the current land difficulty
-        """
-        land_difficulty = driver.execute_script(f'var difficulty = await getLandDifficulty("{self.account}"); return difficulty')
-        return land_difficulty
-
-
-    def get_difficulty(self) -> int:
-        """
-        Returns an integer indicating the current total difficulty
-        """
-        difficulty = self.get_bag_difficulty() + self.get_land_difficulty()
-        return difficulty
-
-
-    def get_last_mine_tx(self) -> str:
-        """
-        Returns the last mine transaction as a string
-        """
-        last_mine_tx = driver.execute_script(f'const last_mine_tx = await lastMineTx("{self.mining_account}", "{self.account}", wax.api.rpc); return last_mine_tx;')
-        return last_mine_tx
-
-
-    def claim(self, mine_work):
-        """
-        Submits the mine_work data to the wax.api for claiming
-        """
-        mine_data = {
-            'miner': mine_work['account'],
-            'nonce': mine_work['rand_str']
+def get_wax_balance(driver, account:str):
+    script = """
+    const val = await fetch(
+        'https://chain.wax.io/v1/chain/get_table_rows', 
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                'json': true,
+                'code': 'eosio.token',
+                'table': 'accounts',
+                'scope': '""" + account + """',
+                'limit': 1000
+            }),  
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'accept': 'application/json, text/plain, */*',
+                'origin': 'https://wallet.wax.io',
+                'referer': 'https://wallet.wax.io'  
+            }
         }
-        try:
-            claimData = driver.execute_script(f'const claimData = await claim("{self.mining_account}","{self.account}", "active", {mine_data}, {self.hyperion_endpoints}, wax.api); return claimData;')
-        except:
-            pass
-        else:
-            print(claimData)
-        finally:
-            print('Finished claiming.')
+    ).then(res => res.json());return val;
+    """
+    # execute JavaScript in chrome driver
+    resp = driver.execute_script(script)
+    assert "rows" in resp
+    return resp["rows"][0]["balance"]
 
 
-    def mine(self, max_tries:int=10) -> dict:
-        """
-        Returns a dictionary containing the mine_work data from current mining transaction
-        """
-        while max_tries:
-            try:
-                mine_work = driver.execute_script('var mine = doWorkWorker({ mining_account: "' + self.mining_account + '", account: "'+ self.account + '", difficulty: ' + str(self.get_difficulty()) + ', last_mine_tx: "' + self.get_last_mine_tx() + '" }); return mine;')
-            except:
-                max_tries -= 1
-            else:
-                break
-        return mine_work
+def get_tlm_balance(driver, account:str):
+    script = """
+    const val = await fetch(
+        'https://chain.wax.io/v1/chain/get_table_rows', 
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                'json': true,
+                'code': 'alien.worlds',
+                'table': 'accounts',
+                'scope': '""" + account + """',
+                'limit': 1000
+            }),  
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'accept': 'application/json, text/plain, */*',
+                'origin': 'https://wallet.wax.io',
+                'referer': 'https://wallet.wax.io'  
+            }
+        }
+    ).then(res => res.json());return val;
+    """
+    # execute JavaScript in chrome driver
+    resp = driver.execute_script(script)
+    assert "rows" in resp
+    return resp["rows"][0]["balance"]
 
 
-    def get_bounty(self) -> str:
-        """
-        Returns a string containing the amount in TLM mined from the last mine transaction
-        """
-        bounty = driver.execute_script(f'const claimBounty = await getBountyFromTx("{self.get_last_mine_tx()}", "{self.account}", {self.hyperion_endpoints}); return claimBounty;')
-        return bounty
+def get_account_stats(driver, account:str) -> dict:
+    script = """
+    const val = await fetch(
+        'https://chain.wax.io/v1/chain/get_account', 
+        {
+            method: 'POST',
+            body: JSON.stringify({account_name: '""" + account + """'}),  
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'origin': 'https://wallet.wax.io',
+                'referer': 'https://wallet.wax.io'  
+            }
+        }
+    ).then(res => res.json());return val;
+    """
+    # execute JavaScript in chrome driver
+    return driver.execute_script(script)
 
 
 def main():
-    # create new AlienWorlds instance
-    aw = AlienWorlds()
+    # define game url
+    url = "https://play.alienworlds.io"
 
-    time.sleep(5)
+    # define range for loop delay
+    delay_min = 60          # min delay before next loop
+    delay_max = 300         # max delay before next loop
 
-    # login to game
-    print("Logging in to game...")
-    aw.login()
+    # define resource threshold
+    resource_limit = 95     # skip mining if resources are above the set limit
+    resource_sleep = 300    # sleep for given time if there is not enough resources
 
-    input("\n[Press ENTER to start]\n")
-    # get bag details
-    bag = aw.get_bag()
+    # set Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--ignore-certificate-errors-spki-list")
+    chrome_options.add_argument("--ignore-ssl-errors")
+    # when there is already a persistent session, you may activate headless mode
+    # chrome_options.add_argument('--headless')
 
-    # print bag details
-    print(f"Bag:")
-    for item in bag:
-        asset_id = item['asset_id']
-        name = item['data']['name']
-        print(f'[{asset_id}]: {name}')
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option("useAutomationExtension", False)
 
-    # get land details
-    land = aw.get_land()
+    # save current browsing session to make it persistent
+    pwd = pathlib.Path().absolute()
+    chrome_options.add_argument(f"--user-data-dir={pwd}\\chrome-data")
 
-    print(f'\nNow mining at {land["name"]} [{land["immutable_data"]["x"]}, {land["immutable_data"]["y"]}] owned by {land["owner"]}')
-    print(f'Commission rate: {land["data"]["commission"] / 100:0.2f}%')
+    # instantiate Chrome driver with given Chrome options
+    driver = webdriver.Chrome(
+        options=chrome_options,
+    )
 
-    # initialize total mined this session
-    mined = 0.0
+    # change page load timeout
+    driver.set_page_load_timeout(10)
 
-    delay = aw.get_mine_delay()
+    # instatiate stealth        
+    stealth(
+        driver,                
+        languages=["en-US", "en"],                
+        vendor="Google Inc.",                
+        platform="Win32",                
+        webgl_vendor="Intel Inc.",                
+        renderer="Intel Iris OpenGL Engine",                
+        fix_hairline=True,        
+    )
 
-    input("\n[Press ENTER to continue]\n")
+    # save current window handle
+    main_window = driver.current_window_handle
 
-    # mining loop
-    for _ in range(100):
-        # get TLM balance
-        tlm = aw.get_balance()
-        print(f"Balance: {tlm}")
+    # make GET request
+    driver.get(url)
 
-        if delay:
-            wait_time = (delay / 1000)
-            wait_time += random.randint(1, wait_time // 10)
-            print(f'\n[Waiting for {wait_time} seconds]\n')
-            time.sleep(wait_time)
+    account = input("Please enter account name (e.g abcde.wam): ")
+    
+    input("\nPlease login and press <ENTER> when ready to Mine")
 
-        # start mining
-        print('Mining...')
-        t0 = timer()
-        mine_work = aw.mine()
-        t1 = timer()
-        nonce = mine_work['rand_str']
-        print(f"Nonce: {nonce} found after {t1 - t0:0.3f} seconds")
+    print("\nStarting bot.\n")
 
-        # create new thread for claiming
-        claim = Thread(target=aw.claim, args=(mine_work,))
-        print('Claiming thread started.')
-        claim.start()
+    # main bot loop
+    for loop_count in count(1):
+        # fetch account stats
+        stats = get_account_stats(driver, account)
 
-        time.sleep(1)
+        # cpu details
+        cpu_max = stats["cpu_limit"]["max"]
+        cpu_used = stats["cpu_limit"]["used"]
+        cpu_pct = int(cpu_used / cpu_max * 100)
 
-        print('Trying to find new window.')
-        # find window of pop-up and switch to that window
+        # ram details
+        ram_max = stats["ram_quota"]
+        ram_used = stats["ram_usage"]
+        ram_pct = int(ram_used / ram_max * 100)
+
+        # net details
+        net_max = stats["net_limit"]["max"]
+        net_used = stats["net_limit"]["used"]
+        net_pct = int(net_used / net_max * 100)
+
+        # line width
+        width = 80
+
+        print("=" * width)
+        print("CPU: [ {:,} / {:,} ms ]\t\t\tUsed: {} %".format(cpu_used, cpu_max, cpu_pct))
+        print("NET: [ {:,} / {:,} B ]\t\t\tUsed: {} %".format(net_used, net_max, net_pct))
+        print("RAM: [ {:,} / {:,} B ]\t\t\tUsed: {} %".format(ram_used, ram_max, ram_pct))
+        print("=" * width)
+
+        # get balances
+        wax = get_wax_balance(driver, account)
+        tlm = get_tlm_balance(driver, account)
+        
+        # show balances
+        print("WAX Balance:", wax)
+        print("TLM Balance:", tlm)
+
+        if (cpu_pct > resource_limit) or (ram_pct > resource_limit) or (net_pct > resource_limit):
+            print("\nResource utilization is above the set threshold of {} %".format(resource_limit))
+            print("\nSleeping for {} seconds\n".format(resource_sleep))
+            time.sleep(resource_sleep)
+            continue        
+
+        # wait for mine button to be found
+        print("\nWaiting for Mine button")
+
+        while True:
+            _print_(".")
+            # try to find mine button
+            try:
+                mine_btn = driver.find_element(By.XPATH, '//*[text()="Mine"]')
+
+            # if button is not found
+            except NoSuchElementException:
+                time.sleep(1)
+
+            # if button is found
+            else:
+                _print_(":")
+                print("\nFound Mine button!")
+                # click
+                mine_btn.click()
+                break
+
+        # wait for claim button
+        claim_btn = WebDriverWait(driver, 60).until(ec.visibility_of_element_located((By.XPATH, '//*[text()="Claim Mine"]')))
+        print("Found Claim button!")
+
+        # click claim button
+        claim_btn.click()
+
+        # pause for 3 seconds
+        time.sleep(3)
+
+        # switch control to pop-up window
         for this_window in driver.window_handles:
             if this_window != main_window:
-                print('Switching to new window.')
+                print('\n\tSwitching to new window.')
                 driver.switch_to.window(this_window)
                 break
 
         # wait for approve button to be visible and click button
         btn = WebDriverWait(driver, 60).until(ec.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'Approve')]")))
-        if btn:
-            print('Found Approve button.')
-            btn.click()
-            print('Approving transaction.')
-        else:
-            raise IOError('Unable to load or find approve button.')
 
-        # go back to main window
-        print('Switching back to main window.')
+        # if approve button is found
+        if btn:
+            print('\tFound Approve button.')
+            btn.click()
+            print('\tApproving transaction.')
+
+        # if approve button could not be found
+        else:
+            raise IOError('\tUnable to load or find approve button.')
+
+        # go control back to main window
+        print('\tSwitching back to main window.\n')
         driver.switch_to.window(main_window)
 
-        print('Claiming done.')
+        # show the number of loops done
+        print(f"Total runs: {loop_count}")
 
-        bounty = aw.get_bounty()
-        delay = aw.get_mine_delay()
-        print(f'Mined: {bounty}\nTime to next mine: {delay / 1000} seconds')
-        amount, unit = bounty.split()
-        amount = float(amount)
-        mined += amount
-        print(f'Total mined this session: {mined} TLM')
+        # delay the bot before starting next loop
+        delay = random.randint(delay_min, delay_max)
+        print(f"\nSleeping for {delay} seconds.")
+        time.sleep(delay)
+        print("\nDone sleeping.\n")
 
 
 if __name__ == '__main__':
@@ -280,7 +270,8 @@ if __name__ == '__main__':
     if not has_chromedriver():
         # change to Release.BETA to use beta releases
         # change to Release.STABLE to use stable releases
-        download_chromedriver(Release.AUTO_DETECT)
+        download_chromedriver()
 
     # call main routine
     main()
+
